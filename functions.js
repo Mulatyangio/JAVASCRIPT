c59ad2e0ff8f99c8a419daa2320aa06fbfde43da
naomi <tarrusnaomi@gmail.com>
2024-05-23 13:01:28 +0300

    ngio

--- functions.js
+++ functions.js
@@ -17,3 +17,15 @@ checkAge(2024, "John");
 //Write a function called calculateTotal that takes two numbers (prices) as input. Inside the function,add the two numbers together. If the sum is greater than 100, apply a 10% discount by multiplying the sum by 0.9. Return the final price after the discount if applicable
 
 //Write a function called isPositiveNumber that takes a number as an input. inside the function, use a logical or operator(||)to check if the number is greater than zero(positive)or equal to zero. If it is return true .Otherwise return false
+
+
+//control structures -> Conditional statements (ternary operator,if else,switch case)and loops (for loop,for in,for of,while, do while )
+//objects---arrays
+//hof
+//async js
+
+//DOM-browser js
+//practice(alot)
+
+//sql/r database ms
+//server js /node/express/template/authentication/authorization/encryption/middleware/
